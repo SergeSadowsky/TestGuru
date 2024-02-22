9113d12e0a44b0a4ae51d56bcad7ad4f69da25bb
uncleserge <serge@sadowsky.spb.ru>
2024-02-22 18:27:17 +0300

    feat: added test_attempt model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_02_21_112346) do
+ActiveRecord::Schema.define(version: 2024_02_22_152156) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -35,6 +35,17 @@ ActiveRecord::Schema.define(version: 2024_02_21_112346) do
     t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
+  create_table "test_attempts", force: :cascade do |t|
+    t.string "status"
+    t.string "result"
+    t.integer "user_id", null: false
+    t.integer "test_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["test_id"], name: "index_test_attempts_on_test_id"
+    t.index ["user_id"], name: "index_test_attempts_on_user_id"
+  end
+
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
     t.integer "level", default: 0, null: false
@@ -54,5 +65,7 @@ ActiveRecord::Schema.define(version: 2024_02_21_112346) do
 
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
+  add_foreign_key "test_attempts", "tests"
+  add_foreign_key "test_attempts", "users"
   add_foreign_key "tests", "categories"
 end
