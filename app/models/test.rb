be8575e403d04608a5d220da0a2949106be56dbf
uncleserge <serge@sadowsky.spb.ru>
2024-02-22 22:28:43 +0300

    feat: created method tests_by_level for User model, created class method by_category for Test class
    Создайте инстанс-метод в модели User, который принимает в качестве аргумента значение уровня сложности и возвращает список всех Тестов, которые проходит или когда-либо проходил Пользователь на этом уровне сложности
    
    Создайте метод класса в модели Test, который возвращает отсортированный по убыванию массив названий всех Тестов у которых Категория называется определённым образом (название категории передается в метод в качестве аргумента).

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,8 @@
 class Test < ApplicationRecord
+  has_many :test_attempts
+  belongs_to :category
+  def self.by_category(category)
+    Category.find_by(title: category).tests.order(title: :desc).pluck(:title)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :test_attempts
+  def tests_by_level(level)
+    Test.joins(:test_attempts).where('test_attempts.user_id = ?', id).where(level: level)
+  end
 end
