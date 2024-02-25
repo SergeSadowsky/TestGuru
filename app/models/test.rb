7fdb6fa156438930684c831a19742bbef6882e0f
uncleserge <serge@sadowsky.spb.ru>
2024-02-25 03:28:21 +0300

    fix: Test.by_category - corrected database request to decrease queries quantity, user.test_by_level - syntax fixed

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,7 +2,8 @@ class Test < ApplicationRecord
   has_many :test_attempts
   belongs_to :category
   def self.by_category(category)
-    Category.find_by(title: category).tests.order(title: :desc).pluck(:title)
+    # Category.find_by(title: category).tests.order(title: :desc).pluck(:title)
+    Test.joins(:category).where(categories: {title: category}).pluck(:title)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :test_attempts
   def tests_by_level(level)
-    Test.joins(:test_attempts).where('test_attempts.user_id = ?', id).where(level: level)
+    # Test.joins(:test_attempts).where('test_attempts.user_id = ?', id).where(level: level)
+    Test.joins(:test_attempts).where(test_attempts: { user_id: id}, level: level)
   end
 end
