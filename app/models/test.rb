a9e0ec7fc903670465a7f2baaf7f92f7079a7709
uncleserge <serge@sadowsky.spb.ru>
2024-02-27 03:53:23 +0300

    feat: added associations Test - Categories, Test - Questions, Question - Answers, Test - Tested Users

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,7 @@
 class Test < ApplicationRecord
   has_many :test_attempts
+  has_many :users, through: :test_attempts
+  has_many :questions
   belongs_to :category
   def self.by_category(category)
     # Category.find_by(title: category).tests.order(title: :desc).pluck(:title)
