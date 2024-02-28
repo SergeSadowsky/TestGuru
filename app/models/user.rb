4208143a0049527a8c6f5a4d8dfea3067055fb2e
uncleserge <serge@sadowsky.spb.ru>
2024-02-29 00:29:17 +0300

    fix: refactored associations User -> Test and method User.tests_by_level

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   has_many :test_attempts
-  has_many :tests, foreign_key: "author_id"
+  has_many :tests, through: :test_attempts
+
+  has_many :authored_tests, class_name: 'Test', foreign_key: "author_id"
+
   def tests_by_level(level)
-    # Test.joins(:test_attempts).where('test_attempts.user_id = ?', id).where(level: level)
-    Test.joins(:test_attempts).where(test_attempts: { user_id: id}, level: level)
+    tests.where(level: level)
   end
 end
