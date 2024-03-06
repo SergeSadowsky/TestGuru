1050d3227c967e5bfa6719ac36e35d4c3cda89bc
uncleserge <serge@sadowsky.spb.ru>
2024-03-06 11:33:00 +0300

    fix: create method, instance variables, params check

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,40 +4,40 @@ class QuestionsController < ApplicationController
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
   def index
-    result = self.test.questions.pluck(:body)
+    result = @test.questions.pluck(:body)
     render html: output(result).html_safe
   end
 
   def show
-    render plain: question.body
+    render plain: @question.body
   end
 
   def new; end
 
   def create
-    question = Question.create(question_params)
-    question.test_id = test.id
-    question.save
-    result = ["Q.id: #{question.id}", "Q.body: #{question.body}", "for Test: #{test.id}"]
+    question = @test.questions.new(question_params)
+    result = if question.save
+               ["Question.id: #{question.id}", "Question.body: #{question.body}", "Question for Test: #{question.test_id}"]
+             else
+               ['Error:', "Can't create question", question.errors.full_message]
+             end
     render html: output(result).html_safe
   end
 
   def destroy
-    self.question.destroy
+    @question.destroy
 
-    render plain: "Question #{self.question.id} deleted"
+    render plain: "Question #{@question.id} deleted"
   end
 
   private
 
-  attr_accessor :test, :question
-
   def find_test
-    self.test = Test.find(params[:test_id])
+    @test = Test.find(params[:test_id])
   end
 
   def find_question
-    self.question = Question.find(params[:id])
+    @question = Question.find(params[:id])
   end
 
   def question_params
